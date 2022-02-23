ca6c92e475588225f58852edc469c1d343670d17
CuongHD <cuong.hoangduc@cxview.ai>
2022-02-23 16:17:39 +0000

    refresh control

--- App.tsx
+++ App.tsx
@@ -1,84 +1,48 @@
-import React, { useState } from 'react';
-import { Alert, Modal, StyleSheet, View, Text, Pressable } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Alert, Modal, StyleSheet, View, Text, Pressable, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
+
+const wait = (timeout: number | undefined) => {
+    return new Promise(resolve => setTimeout(resolve, timeout))
+}
 
 const App = () => {
-    const [modalVisible, setModalVisible] = useState(false);
+    const [refreshing, setRefreshing] = useState(false)
+    
+    const onRefresh = useCallback(() => {
+        setRefreshing(true)
+        wait(2000).then(() => setRefreshing(false))
+    }, [])
 
     return (
-        <View style={styles.centeredView}>
-            <Modal
-                animationType='fade'
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    Alert.alert('Modal has been closed');
-                    setModalVisible(!modalVisible);
-                }}
+        <SafeAreaView style={styles.container}>
+            <ScrollView 
+                contentContainerStyle={styles.scrollView}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    >
+
+                    </RefreshControl>
+                }
             >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello world</Text>
-                        <Pressable
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={() => setModalVisible(!modalVisible)}
-                        >
-                            <Text style={styles.textStyle}>Hide Modal</Text>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
-            <Pressable
-                style={[styles.button, styles.buttonOpen]}
-                onPress={() => setModalVisible(true)}
-            >
-                <Text style={styles.textStyle}>Show Modal</Text>
-            </Pressable>
-        </View>
+                <Text>Pull down to refresh</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
+    container: {
+      flex: 1,
     },
-    modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
+    scrollView: {
+      flex: 1,
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-    },
-    buttonOpen: {
-        backgroundColor: '#F194FF',
-    },
-    buttonClose: {
-        backgroundColor: '#2196F3',
-    },
-    textStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
-    },
-});
+  });
+  
 
 export default App;
