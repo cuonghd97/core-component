64f1345bb03ef85108be3a1a128761a05414a52f
CuongHD <cuong.hoangduc@cxview.ai>
2022-02-25 15:51:02 +0000

    section-list

--- App.tsx
+++ App.tsx
@@ -1,46 +1,67 @@
-import React, { useCallback, useState } from 'react';
-import { Alert, Modal, StyleSheet, View, Text, Pressable, SafeAreaView, ScrollView, RefreshControl, StatusBar } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
 
-const wait = (timeout: number | undefined) => {
-    return new Promise(resolve => setTimeout(resolve, timeout))
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"]
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"]
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"]
+  }
+];
+
+interface Props {
+    title: string
 }
 
-const App = () => {
+const Item: React.FC<Props> = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView
-                style={styles.scrollView}
-                alwaysBounceHorizontal={true}
-                decelerationRate="normal"
-                scrollEnabled={true}
-                showsHorizontalScrollIndicator={false}
-                showsVerticalScrollIndicator={false}
-            >
-                <Text style={styles.text}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt consequuntur ea esse autem, at laboriosam. Ducimus vero molestias laudantium quo facere porro dignissimos sed. Iusto ipsa provident dolores fugit.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat consectetur aliquid amet, maxime ex, aut perspiciatis tempora aperiam, est eos vitae labore autem. Adipisci nisi cum, cumque sapiente vitae explicabo.
-                </Text>
-            </ScrollView>
-
-        </SafeAreaView>
-    );
-};
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+      inverted={false}
+    />
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: StatusBar.currentHeight,
-    },
-    scrollView: {
-        backgroundColor: 'pink',
-        marginHorizontal: 20,
-    },
-    text: {
-        fontSize: 42,
-    },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff"
+  },
+  title: {
+    fontSize: 24
+  }
 });
-
 
 export default App;
