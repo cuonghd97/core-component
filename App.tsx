578621f3afa909a5145fe9de81acce0cb90dc22a
CuongHD <cuong.hoangduc@cxview.ai>
2022-02-25 15:24:25 +0000

    scroll-view

--- App.tsx
+++ App.tsx
@@ -1,48 +1,46 @@
 import React, { useCallback, useState } from 'react';
-import { Alert, Modal, StyleSheet, View, Text, Pressable, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
+import { Alert, Modal, StyleSheet, View, Text, Pressable, SafeAreaView, ScrollView, RefreshControl, StatusBar } from 'react-native';
 
 const wait = (timeout: number | undefined) => {
     return new Promise(resolve => setTimeout(resolve, timeout))
 }
 
 const App = () => {
-    const [refreshing, setRefreshing] = useState(false)
-    
-    const onRefresh = useCallback(() => {
-        setRefreshing(true)
-        wait(2000).then(() => setRefreshing(false))
-    }, [])
+
 
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView 
-                contentContainerStyle={styles.scrollView}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                    >
-
-                    </RefreshControl>
-                }
+            <ScrollView
+                style={styles.scrollView}
+                alwaysBounceHorizontal={true}
+                decelerationRate="normal"
+                scrollEnabled={true}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
             >
-                <Text>Pull down to refresh</Text>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt consequuntur ea esse autem, at laboriosam. Ducimus vero molestias laudantium quo facere porro dignissimos sed. Iusto ipsa provident dolores fugit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat consectetur aliquid amet, maxime ex, aut perspiciatis tempora aperiam, est eos vitae labore autem. Adipisci nisi cum, cumque sapiente vitae explicabo.
+                </Text>
             </ScrollView>
+
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
     },
     scrollView: {
-      flex: 1,
-      backgroundColor: 'pink',
-      alignItems: 'center',
-      justifyContent: 'center',
+        backgroundColor: 'pink',
+        marginHorizontal: 20,
     },
-  });
-  
+    text: {
+        fontSize: 42,
+    },
+});
+
 
 export default App;
