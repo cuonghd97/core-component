7fa90ae28f4cd16d778df94fbfe0a40e2e1edd47
CuongHD <cuong.hoangduc@cxview.ai>
2022-03-02 15:01:57 +0000

    text

--- App.tsx
+++ App.tsx
@@ -1,32 +1,23 @@
 import React, { useState } from 'react';
 import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Switch, Text, View } from 'react-native';
 
-
 const App = () => {
-    const [isEnabled, setIsEnabled] = useState(false)
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState)
-
-  return (
-    <View style={styles.container}>
-        <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-        >
-            
-        </Switch>
-    </View>
-  );
+    return (
+        <View accessible={true}>
+            <Text>text one</Text>
+            <Text>text two</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center"
+    baseText: {
+        fontWeight: 'bold',
+        fontSize: 50
+    },
+    innerText: {
+        color: 'red'
     }
-  });
+});
 
 export default App;
