ec0b81e91eb34ac44043e4dced95a227c39606bd
DESKTOP-E8OTJ33\Cuong <cuong.hoangduc@cxview.ai>
2022-03-03 16:02:05 +0000

    virtualized-list

--- App.tsx
+++ App.tsx
@@ -1,51 +1,65 @@
 import React, { useState } from 'react';
-import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Switch, Text, View, TouchableHighlight } from 'react-native';
+import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Switch, Text, View, TouchableHighlight, VirtualizedList, ListRenderItem } from 'react-native';
 
 
-const TouchableHightlightExample = () => {
-    const [count, setCount] = useState(0)
-    const onPress =     () => setCount(count+1)
-    return (
-        <View style={styles.container}>
-            <TouchableHighlight  onPress={onPress} activeOpacity={0.5} underlayColor={"#ffebcd"}>
-                <View style={styles.button}>
-                    <Text>Touch Here</Text>
-                </View>
-            </TouchableHighlight>
-            <View style={styles.countContainer}>
-                <Text style={styles.countText}>
-                    {count||null}
-                </Text>
-            </View>
-        </View>
-    )
+
+type ItemModel = {
+    id: string,
+    title: string
 }
+
+const DATA: ItemModel[] = []
+
+const getItem = (data: Array<ItemModel>, index: number) => ({
+    id: Math.random().toString(12).substring(0),
+    title: `Item ${index + 1}`
+})
+
+const getItemCount = (data: Array<ItemModel>) => 50
+
+
+const Item: React.FC<{ title: String }> = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+)
+
 
 const App = () => {
 
-  return (
-    <TouchableHightlightExample/>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <VirtualizedList<ItemModel>
+                data={DATA}
+                initialNumToRender={4}
+                renderItem={({ item }) => <Item title={item.title}></Item>}
+                keyExtractor={(item) => item.id}
+                getItemCount={getItemCount}
+                getItem={getItem}
+                horizontal={false}
+            >
+
+            </VirtualizedList>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10
-  },
-  countText: {
-    color: "#FF00FF"
-  }
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        height: 150,
+        justifyContent: 'center',
+        marginVertical: 8,
+        marginHorizontal: 16,
+        padding: 20,
+    },
+    title: {
+        fontSize: 32,
+    },
 });
 
 export default App;
