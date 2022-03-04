cf05873a83b18fc93f53aa9d25b1994882336d33
DESKTOP-E8OTJ33\Cuong <cuong.hoangduc@cxview.ai>
2022-03-04 15:09:18 +0000

    drawer-layout-android

--- App.tsx
+++ App.tsx
@@ -1,65 +1,69 @@
-import React, { useState } from 'react';
-import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Switch, Text, View, TouchableHighlight, VirtualizedList, ListRenderItem } from 'react-native';
-
-
-
-type ItemModel = {
-    id: string,
-    title: string
-}
-
-const DATA: ItemModel[] = []
-
-const getItem = (data: Array<ItemModel>, index: number) => ({
-    id: Math.random().toString(12).substring(0),
-    title: `Item ${index + 1}`
-})
-
-const getItemCount = (data: Array<ItemModel>) => 50
-
-
-const Item: React.FC<{ title: String }> = ({ title }) => (
-    <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-    </View>
-)
-
+import React, { useRef, useState } from "react";
+import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from "react-native";
 
 const App = () => {
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState("left");
+  const changeDrawerPosition = () => {
+    if (drawerPosition === "left") {
+      setDrawerPosition("right");
+    } else {
+      setDrawerPosition("left");
+    }
+  };
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <VirtualizedList<ItemModel>
-                data={DATA}
-                initialNumToRender={4}
-                renderItem={({ item }) => <Item title={item.title}></Item>}
-                keyExtractor={(item) => item.id}
-                getItemCount={getItemCount}
-                getItem={getItem}
-                horizontal={false}
-            >
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
+    </View>
+  );
 
-            </VirtualizedList>
-        </SafeAreaView>
-    );
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={drawerPosition}
+      renderNavigationView={navigationView}
+    >
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Drawer on the {drawerPosition}!
+        </Text>
+        <Button
+          title="Change Drawer Position"
+          onPress={() => changeDrawerPosition()}
+        />
+        <Text style={styles.paragraph}>
+          Swipe from the side or press button below to see it!
+        </Text>
+        <Button
+          title="Open drawer"
+          onPress={() => drawer.current?.openDrawer()}
+        />
+      </View>
+    </DrawerLayoutAndroid>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight,
-    },
-    item: {
-        backgroundColor: '#f9c2ff',
-        height: 150,
-        justifyContent: 'center',
-        marginVertical: 8,
-        marginHorizontal: 16,
-        padding: 20,
-    },
-    title: {
-        fontSize: 32,
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16
+  },
+  navigationContainer: {
+    backgroundColor: "#ecf0f1"
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: "center"
+  }
 });
 
 export default App;
